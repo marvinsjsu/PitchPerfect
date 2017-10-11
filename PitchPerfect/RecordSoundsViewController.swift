14e230c804a55eaa854f30abb874c3d1a0ebf593
Marvin Mante <mmante@sugarcrm.com>
2017-10-10 22:29:34 -0700

    removed self - so UIViewController has present method and we didn't need to use self to refer to our instance, rather just call the method we have in it

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -167,7 +167,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     func showAlert(_ title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: Alerts.dismissAlert, style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
 }
 
